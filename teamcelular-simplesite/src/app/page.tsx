8ae134a880de020fe12e3a2c06d5142bd4da6b10
Reinapepeada <alejandrobiarrieta@gmail.com>
2024-02-06 11:17:54 -0300

    crando next e integrando nextui

---
+++ teamcelular-simplesite/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Team Celular</h1>
+      <p>
+        
+      </p>
+    </main>
+  );
+}
