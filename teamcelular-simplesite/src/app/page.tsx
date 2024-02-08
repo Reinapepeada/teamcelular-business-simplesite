6e57afc3641fe0f0e684beae307a1f2bc2462163
Alejandro Biarrieta <103573136+Reinapepeada@users.noreply.github.com>
2024-02-08 18:11:59 -0300

    agregando cards al home

--- teamcelular-simplesite/src/app/page.tsx
+++ teamcelular-simplesite/src/app/page.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import BannerHome from "../components/banners/BannerHome"
+import BannerCards from "@/components/cards/BannerCards"
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-8 m-4 flex flex-col justify-center justify-items-center">
       <BannerHome />
+      
+      <BannerCards />
     </div>
     
   )
--- teamcelular-simplesite/src/components/banners/BannerHome.tsx
+++ teamcelular-simplesite/src/components/banners/BannerHome.tsx
@@ -4,18 +4,19 @@ import CoverImageCard from "@/components/cards/CoverImageCard";
 import CoverLeguend from "../cards/CoverLeguend";
 import CoverLeguend2 from "../cards/CoverLeguend2";
 import { Oswald } from "next/font/google";
-const oswald = Oswald({ subsets: ["latin"], weight:'400' });
+const oswald = Oswald({ subsets: ["latin"], weight: "400" });
 
 export default function BannerHome() {
     return (
-        <section className="flex flex-col 
+        <section
+            className="flex flex-col 
         
         justify-center
         items-center
         space-y-4
         ">
-
-            <div className="object-cover flex justify-center 
+            <div
+                className="object-cover flex justify-center 
             
             w-3/4
             md:w-1/2
@@ -24,10 +25,15 @@ export default function BannerHome() {
                 <ReparacionCelular />
             </div>
             <div className="space-y-1 flex flex-col items-start md:flex md:flex-row md:space-x-2 md:items-end">
-
-            <h2 className={`${oswald.className} text-5xl light:text-black`} >La</h2>
-            <h2 className={`${oswald.className} text-5xl light:text-black`} >EFICIENCIA</h2>
-            <h2 className={`${oswald.className} text-5xl light:text-black`} >es nuestro norte.</h2>
+                <h2 className={`${oswald.className} text-5xl light:text-black`}>
+                    La
+                </h2>
+                <h2 className={`${oswald.className} text-5xl light:text-black`}>
+                    EFICIENCIA
+                </h2>
+                <h2 className={`${oswald.className} text-5xl light:text-black`}>
+                    es nuestro norte.
+                </h2>
             </div>
         </section>
     );
