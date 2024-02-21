c50ed9cbcfdbe6b654bf5eb16666443d3438ce57
Reinapepeada <alejandrobiarrieta@gmail.com>
2024-02-21 12:55:31 -0300

    actualizacion footer

--- teamcelular-simplesite/src/components/footer/FooterNUI.tsx
+++ teamcelular-simplesite/src/components/footer/FooterNUI.tsx
@@ -3,46 +3,62 @@ import React from "react";
 import { Sora } from "next/font/google";
 const sora = Sora({ subsets: ["latin"], weight: "300" });
 
-import { Divider } from "@nextui-org/react";
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    Divider,
+    Input,
+    Button,
+} from "@nextui-org/react";
 
 export default function FooterNUI() {
     return (
         <footer className="flex bg-footer-background place-center text-white justify-center py-9">
             <div className="flex flex-col justify-center w-2/3 m-4 ">
-                <div className="flex flex-row justify-between h-64">
-                    <p className="text-tiny">© 2024 Team Celular</p>
-                    <div className="flex flex-row justify-center items-start space-x-12">
-                        <Divider orientation="vertical" />
-                        <div className="flex flex-col justify-center items-start">
-                            <h6 className="text-2xl font-bold"> Servicios</h6>
+                <div className="flex flex-col md:flex-row md:text-start justify-between items-center text-center my-4">
+                    <Card shadow="lg">
+                        <CardHeader className="text-2xl font-bold m-2">
+                            Recibe ofertas exclusivas y novedades
+                        </CardHeader>
+                        <CardBody className="text-sm p-4 space-y-4">
+                            <p className="ml-1">Suscribite a nuestro CLUB</p>
+                            <Input type="text" placeholder="Ingresa tu email" />
+                            <Button className="">Suscribirme</Button>
+                        </CardBody>
+                    </Card>
+                    <Divider
+                        className="hidden md:inline"
+                        orientation="vertical"
+                    />
+                    <div className="flex flex-col items-center md:space-x-12 md:flex-row md:items-start">
+                        <div className="flex flex-col justify-center items-center md:items-start">
+                            <h6 className="text-2xl font-bold">Servicios</h6>
                             <div className={`${sora.className} space-y-2 mt-2`}>
                                 <h6 className="text-sm">Reparaciones</h6>
                                 <h6 className="text-sm">Gremio</h6>
                                 <h6 className="text-sm">Empresas</h6>
                                 <h6 className="text-sm">Soporte técnico</h6>
-                                <h6 className="text-sm">
-                                    Instalaciones
-                                </h6>
+                                <h6 className="text-sm">Instalaciones</h6>
                             </div>
                         </div>
-                        <div className="flex flex-col justify-center items-start">
-                            <h6 className="text-2xl font-bold"> Productos</h6>
-                            <div className="space-y-2 mt-2">
+                        <div className="flex flex-col justify-center items-center md:items-start">
+                            <h6 className="text-2xl font-bold">Productos</h6>
+                            <div className={`${sora.className} space-y-2 mt-2`}>
                                 <h6 className="text-sm">Accesorios</h6>
                                 <h6 className="text-sm">Electronica</h6>
                                 <h6 className="text-sm">Repuestos</h6>
                             </div>
                         </div>
-                        <div className="flex flex-col justify-center items-start">
+                        <div className="flex flex-col justify-center items-center md:items-start">
                             <h6 className="text-2xl font-bold">Locales</h6>
                             <div className={`${sora.className} space-y-2 mt-2`}>
-
                                 <h6 className="text-sm">Ubicaciones</h6>
                                 <h6 className="text-sm">Citas</h6>
                                 <h6 className="text-sm">Horarios</h6>
                             </div>
                         </div>
-                        <div className="flex flex-col justify-center items-start">
+                        <div className="flex flex-col justify-center items-center md:items-start">
                             <h6 className="text-2xl font-bold">Condiciones</h6>
                             <div className={`${sora.className} space-y-2 mt-2`}>
                                 <h6 className="text-sm">
