e6ec02b59a836aa26345d1322a5619ddc77a2445
Reinapepeada <alejandrobiarrieta@gmail.com>
2024-03-06 19:47:54 -0300

    actualizando select

--- teamcelular-simplesite/src/components/forms/RepairsForm.tsx
+++ teamcelular-simplesite/src/components/forms/RepairsForm.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Input } from "@nextui-org/react";
+import { Input,Select,SelectItem } from "@nextui-org/react";
+import {failures} from "../../data/failures";
 
 export default function RepairForm() {
     return (
         <section className="flex flex-col w-10/12 md:w-3/4  gap-4">
             <div className="flex flex-row justify-center items-center gap-4">
-                <Input type="text" label="Nombre" />
-                <Input type="text" label="Apellido" />
-                <Input placeholder="nombre" type="text" label="Nombre" />
+                <Input type="text" label="Marca" />
+                <Input type="text" label="Modelo" />
+                <Select 
+                    label="Tipo de reparación"
+                    placeholder="Seleccione una opción"
+                >
+                  
+                </Select>
+
             </div>
 
             <div className="flex flex-row justify-center items-center gap-4">
