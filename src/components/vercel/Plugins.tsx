d42a853284bb8f56810fe8696dcb33a1ca7bf8b5
alejandro <alejandrobiarrieta@gmail.com>
2024-04-19 11:06:34 -0300

    quitando speed analitics

--- src/components/vercel/Plugins.tsx
+++ src/components/vercel/Plugins.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function () {
   return (
     <div>
         <Analytics></Analytics>
-        <SpeedInsights></SpeedInsights>
     </div>
   )
 }
