6629eac8191c4b3ca81afdd5f6cdefbdc1d812b4
alejandro <alejandrobiarrieta@gmail.com>
2024-04-19 11:13:58 -0300

    arregalndo

--- src/components/vercel/Plugins.tsx
+++ src/components/vercel/Plugins.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-export default function () {
+export default function Plugins() {
   return (
     <div>
         <Analytics></Analytics>
