707f5ca8930edbe6d437ed43dd0aa7645369ac22
alejandro <alejandrobiarrieta@gmail.com>
2024-04-19 10:57:27 -0300

    update meta form presupues

--- src/app/presupuesto-reparacion/page.tsx
+++ src/app/presupuesto-reparacion/page.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import RepairsForm from "@/components/forms/RepairsForm"; // Fix the casing of the import statement
 import { Oswald } from "next/font/google";
 const oswald = Oswald({ subsets: ["latin"], weight: "500" });
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Presupuesto la instante | Reparacion de celulares y computadoras en Buenos Aires | Team Celular",
+  description: "Crea un presupuesto para reparar tu equipo de manera rápida y sencilla.",
+};
 export default function CreadorPresupuestos() {
   return (
     <section className="flex flex-col justify-center items-center">
