7b95b1c1d15ea5f1ba10b611cf1e2a8b74542a07
alejandro <alejandrobiarrieta@gmail.com>
2024-04-19 11:10:20 -0300

    cambio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,6 @@ export const metadata: Metadata = {
         "Team Celular es una empresa de reparación de celulares en la Ciudad Autonoma de Buenos Aires, Argentina.",
 };
 
-
-
 export default function RootLayout({
     children,
 }: {
@@ -26,32 +24,50 @@ export default function RootLayout({
     return (
         <html lang="es">
             <Head>
-                <title>Reparaciones de Celulares y Computadoras en Buenos Aires | Team Celular</title>
-                <meta name="description" content="Team Celular ofrece servicios de reparación de alta calidad para celulares y computadoras en Buenos Aires. Nuestros técnicos expertos están listos para ayudarte."></meta>
-                <meta name="keywords" content="Reparación de Celulares Buenos Aires, Reparación de Computadoras Buenos Aires, Servicio Técnico Buenos Aires, Team Celular"></meta>
+                <title>
+                    Reparaciones de Celulares y Computadoras en Buenos Aires |
+                    Team Celular
+                </title>
+                <meta
+                    name="description"
+                    content="Team Celular ofrece servicios de reparación de alta calidad para celulares y computadoras en Buenos Aires. Nuestros técnicos expertos están listos para ayudarte."></meta>
+                <meta
+                    name="keywords"
+                    content="Reparación de Celulares Buenos Aires, Reparación de Computadoras Buenos Aires, Servicio Técnico Buenos Aires, Team Celular"></meta>
                 <meta name="robots" content="index, follow"></meta>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"></meta>
                 <meta name="HandheldFriendly" content="True"></meta>
                 <meta name="MobileOptimized" content="320"></meta>
                 <meta httpEquiv="cleartype" content="on"></meta>
-                <meta property="og:title" content="Reparaciones de Celulares y Computadoras en Buenos Aires | Team Celular"></meta>
-                <meta property="og:description" content="Team Celular ofrece servicios de reparación de alta calidad para celulares y computadoras en Buenos Aires. Nuestros técnicos expertos están listos para ayudarte."></meta>
-                <meta property="og:image" content="/images/teamcelular.png"></meta>
-                <meta property="og:url" content="https://teamcelular.com/"></meta>
+                <meta
+                    property="og:title"
+                    content="Reparaciones de Celulares y Computadoras en Buenos Aires | Team Celular"></meta>
+                <meta
+                    property="og:description"
+                    content="Team Celular ofrece servicios de reparación de alta calidad para celulares y computadoras en Buenos Aires. Nuestros técnicos expertos están listos para ayudarte."></meta>
+                <meta
+                    property="og:image"
+                    content="/images/teamcelular.png"></meta>
+                <meta
+                    property="og:url"
+                    content="https://teamcelular.com/"></meta>
                 <meta name="author" content="Team Celular"></meta>
                 <meta name="geo.region" content="AR-C"></meta>
                 <meta name="geo.placename" content="Buenos Aires"></meta>
-                <meta name="geo.position" content="-34.597532;-58.402662"></meta>
+                <meta
+                    name="geo.position"
+                    content="-34.597532;-58.402662"></meta>
                 <meta name="ICBM" content="-34.597532, -58.402662"></meta>
             </Head>
             <link rel="icon" href="/images/teamcelular.png" sizes="any" />
-            <body
-                className={`${inter.className} `}>
-                <Plugins />
+            <body className={`${inter.className} `}>
                 <Providers>
                     <NavbarNUI></NavbarNUI>
                     <BackgroundBeams />
                     <main className="flex justify-center">{children}</main>
+                    <Plugins />
                     <FooterNUI></FooterNUI>
                 </Providers>
             </body>
