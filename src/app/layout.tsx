992b99d6fe4f36608d5343987a951d0068fbd1a5
Reinapepeada <103573136+Reinapepeada@users.noreply.github.com>
2024-11-21 13:30:47 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 import "./globals.css";
 import type { Metadata } from "next";
-import { NextSeo } from "next-seo";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import NavbarNUI from "../components/navbars/NavbarNUI";
