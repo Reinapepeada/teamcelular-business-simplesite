3f19801d664dd1fbffa5e69184ef7c59bf9c0ef0
Alejandro Biarrieta <alejandrobiarrieta@gmail.com>
2026-08-12 14:59:16 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -135,6 +135,13 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-1S64KMRF99');`}
                 </Script>
+                <Script id="microsoft-clarity" strategy="afterInteractive">
+                    {`(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "y1c74jznnm");`}
+                </Script>
             </head>
             <body className={`${inter.className} bg-white text-slate-950 dark:bg-slate-950 dark:text-slate-50`}>
                 <ThemeProvider
